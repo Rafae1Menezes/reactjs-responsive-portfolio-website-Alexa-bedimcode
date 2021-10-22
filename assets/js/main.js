b27f098994b5e3564ef372d2658c278ca3d7b021
Rafael Menezes <menezes.inbox@gmail.com>
2021-10-22 14:18:58 -0300

    Qualifications

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,10 +32,50 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const   skillsContent = document.getElementsByClassName('skills__content'),
+        skillsHeader = document.querySelectorAll('.skills__header')
 
+function toggleSkills(){
+
+    let itemClass = this.parentNode.className
+
+    for(i=0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+
+    if(itemClass == 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
 
 /*==================== QUALIFICATION TABS ====================*/
+const   tabs = document.querySelectorAll('[data-target]'),
+        tabContents = document.querySelectorAll('[data-content]')
 
+console.log(tabs)
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+
+        target.classList.add('qualification__active')
+
+        tab.forEach(tab =>{
+            tab.classList.remove('Qualification__active')
+        })
+
+        tab.classList.add('qualification__active')
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
