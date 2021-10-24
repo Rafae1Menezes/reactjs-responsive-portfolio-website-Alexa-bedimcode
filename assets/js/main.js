f019d8bd581512b0c4cd2d0d42920b00a5e8048d
Rafael Menezes <menezes.inbox@gmail.com>
2021-10-24 10:29:36 -0300

    Project

--- assets/js/main.js
+++ assets/js/main.js
@@ -107,17 +107,17 @@ modalCloses.forEach(modalCLose => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper('.portfolio__container', {
+let swiper = new Swiper('.mySwiper', {
    cssMode: true,
+   loop: true,
    navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
    },
    pagination: {
       el: '.swiper-pagination',
+      clickable: true,
    },
-   mousewheel: true,
-   keyboard: true,
 })
 
 /*==================== TESTIMONIAL ====================*/
