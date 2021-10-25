5d5a27fef70dd898f0386b90c6f1484dbf13c15f
Rafael Menezes <menezes.inbox@gmail.com>
2021-10-25 20:35:20 -0300

    Finalização
    Scroll Sections Active Link, Change Background Heade,  Scroll Up, Dark/Light Theme, Scroll Bar, Media Queries

--- assets/js/main.js
+++ assets/js/main.js
@@ -141,9 +141,75 @@ let swiperTestimonial = new Swiper('.testimonal__container', {
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+   const scrollY= window.pageYOffset
+
+   sections.forEach(current => {
+      const sectionHeight = current.offsetHeight
+      const sectionTop = current.offsetTop - 50
+      const sectionId = current.getAttribute('id')
+
+      if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+         document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+      }else{
+         document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+      }
+   })
+}
+
+window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader(){
+   const nav = document.getElementById('header')
+   // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+   if(this.scrollY >= 80) nav.classList.add('scroll-header')
+   else nav.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/
+function scrollUp(){
+   const scrollUp = document.getElementById('scroll-up');
+   //when the scholl is height than 560 viewpot height, add the show-scroll clas to the tag with the scroll-up id
+   if(this.scrollY >= 560) 
+      scrollUp.classList.add('show-scroll')
+   else
+      scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollUp)
 
 /*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+//Previously selected tpic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+//We obtain the current them that the interface has by validating the dark-them class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+//we validade if the user previousl chose a topic
+if(selectedTheme){
+   //If the validadetion is fulfilled, we ask what the issue was to know if we activaed or deactivated the dark-theme
+   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+   themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+   //Add or remove te dark / icon theme
+   document.body.classList.toggle(darkTheme)
+   themeButton.classList.toggle(iconTheme)
+   //We save the theme and the current icon that the user chose
+   localStorage.setItem('selected-theme', getCurrentTheme())
+   localStorage.setItem('selected-icon', getCurrentIcon())
+
+})
